9a3641714f8018bb49c5a52edf2e4c375cdfa16f
ceciliaTunapanda <cecilia@tunapanda.org>
2015-11-30 14:14:11 +0300

    added breadcrumbs links

--- content.php
+++ content.php
@@ -1,5 +1,23 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<h1><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></h1>
+	<h1><?php //the_title( '<h1 class="entry-title">', '</h1>' ); ?></h1>
+	
+		<?php 
+			if (!is_front_page()){
+				echo '<div id="breadcrumbs">';
+				for ($i = count($post->ancestors)-1; $i >= 0; $i--) {
+					if (($home->ID) != ($post->ancestors[$i])) {
+						echo '<a id="breadcrumbs_links" href="';
+						echo get_permalink($post->ancestors[$i]); 
+						echo '">';
+						echo get_the_title($post->ancestors[$i]);
+						echo "</a>".">>";
+					}
+				}
+				echo get_the_title(get_the_ID());
+				echo '</div>';
+			}
+		?>
+	
 
 	<?php the_content(); ?>
 	<?php
