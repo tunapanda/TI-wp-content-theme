b344328eeb72aa2893cadb48eeda396221a5980f
Neeraj Kamdar <Neeraj@NK.local>
2016-01-06 21:40:09 +0300

    Added favicons and icons for apple devices and windows 10

--- header.php
+++ header.php
@@ -6,6 +6,13 @@
 			THEME_URI='<?php echo get_template_directory_uri(); ?>';
 		</script>
 		<meta name="viewport" content="width=device-width">
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/icons/tunapanda.ico" />
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-57x57-precomposed.png" />
+    	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-72x72-precomposed.png" />
+    	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon-114x114-precomposed.png" />
+    	<meta name="application-name" content="Tunapanda Institute"/>
+    	<meta name="msapplication-TileColor" content="#ffffff"/> 
+    	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/img/icons/ms-icon-144x144.png"/>
 	</head>
 	<body <?php body_class(); ?>>
 		<div id="header">
