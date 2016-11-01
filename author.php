576252d2bef389f8dbaf409a2c644168196e1714
Mikael Lindqvist <li.mikael@gmail.com>
2016-11-01 14:04:28 +0300

    author page

---
+++ author.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<article>
+	<h1></h1><br/>
+	<?php echo do_shortcode("[author-swag]"); ?>
+</article>
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,23 +1,29 @@
 <?php 
 
-	/**
-	* Init.
-	*/
-	function ti_init() {
-		add_post_type_support('page','excerpt');
-		add_post_type_support('page','custom-fields');
-	}
-	add_action("init","ti_init");
-	add_theme_support('menus');
+/**
+* Init.
+*/
+function ti_init() {
+	add_post_type_support('page','excerpt');
+	add_post_type_support('page','custom-fields');
+}
+add_action("init","ti_init");
+add_theme_support('menus');
 
 
-	/**
-	 * Scripts and styles.
-	 */
-	function ti_enqueue_scripts() {
-		wp_register_style("ti",get_template_directory_uri()."/style.css?v=7"); //?v=x added to refresh browser cache when stylesheet is updated. 
-		wp_enqueue_style("ti");
-	}
-	add_action('wp_enqueue_scripts','ti_enqueue_scripts');
-	
-	register_nav_menu("navigation","Main menu for the site");
+/**
+ * Scripts and styles.
+ */
+function ti_enqueue_scripts() {
+	wp_register_style("ti",get_template_directory_uri()."/style.css?v=7"); //?v=x added to refresh browser cache when stylesheet is updated. 
+	wp_enqueue_style("ti");
+}
+add_action('wp_enqueue_scripts','ti_enqueue_scripts');
+
+register_nav_menu("navigation","Main menu for the site");
+
+function ti_redirect_wppb_login() {
+	return do_shortcode("[wppb-login]");
+}
+
+add_shortcode("redirect-wppb-login","ti_redirect_wppb_login");
